83038f4f1708fe974f5b3f83ac6f02b1ac06c89f
LEVLLN <lev.kudryashov1994@gmail.com>
2018-01-27 22:51:36 +0700

    Добавлена модель Content

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Content(models.Model):
+    text = models.TextField()
+    def __str__(self):
+        """A string representation of the model."""
+        return 'text: ' + self.text[:50]
