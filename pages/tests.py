a82b6477f7c643b5f1612c703fb7a761ab4fe2e1
LEVLLN <lev.kudryashov1994@gmail.com>
2018-01-27 22:52:52 +0700

    Unit tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,57 @@
 from django.test import TestCase
-
+from django.test import SimpleTestCase
+from .models import Content
+from django.urls import reverse
 # Create your tests here.
+# pages/tests.py
+
+
+
+class ContentModelTest(TestCase):
+    """docstring for ContentModelTest"""
+    @classmethod
+    def setUpTestData(cls):
+       Content.objects.create(text="The test data")
+
+    def test_text_content(self):
+        content = Content.objects.get(id=1)
+        expected_object_name = f'{content.text}'
+        self.assertEquals(expected_object_name, 'The test data')
+
+class HomePageViewTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Content.objects.create(text='this is another test')
+
+    def test_view_url_exists_at_proper_location(self):
+        resp = self.client.get('/')
+        self.assertEquals(resp.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEquals(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEquals(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'home.html')
+
+class AboutPageViewTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Content.objects.create(text='this is another test')
+
+    def test_view_url_exists_at_proper_location(self):
+        resp = self.client.get('/about/')
+        self.assertEquals(resp.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('about'))
+        self.assertEquals(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('about'))
+        self.assertEquals(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'about.html')
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Content
 
 # Create your views here.
-class HomePageView(TemplateView):
+class HomePageView(ListView):
 	template_name = 'home.html'
+	model = Content
 
 class AboutPageView(TemplateView):
 	template_name = 'about.html'
+	
